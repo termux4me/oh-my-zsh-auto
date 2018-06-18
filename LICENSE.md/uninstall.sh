481e4c664516302e7f695a30b69bac3ac43fb56b
termux4me <36556358+termux4me@users.noreply.github.com>
2018-06-18 22:53:12 +0100

    Create uninstall.sh

---
+++ LICENSE.md/uninstall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+red='\033[1;31m'
+green='\033[1;32m'
+#script by Dz@shell
+#young algerian programmer
+cd $HOME
+printf "$red"
+echo "done"
+rm -fr .oh-my-zsh .termux .termux.bak.2018.06.18-14:49:26
+rm -fr .zshrc.bak.2018.06.18-14:56:31 termux-ohmyzsh
+rm -fr .zcompdump-localhost-5.5.1 .zshrc
+rm -fr .zsh_history .bash_history .config .local
+cd /data/data/com.termux/files/usr/etc
+rm -fr zshrc
+cd $HOME
+printf "$green"
+echo "Now restart termux"
