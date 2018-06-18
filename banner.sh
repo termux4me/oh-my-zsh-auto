2dfdcdaf646c9ab34add7a898305bdad545ad013
Termux <36556358+termux4me@users.noreply.github.com>
2018-06-19 00:10:24 +0100

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+cd /data/data/com.termux/files/usr/etc
+nano zshrc
---
+++ oh-my-zsh.sh
@@ -0,0 +1,4 @@
+#!/bash/usr/bin/env sh
+echo "vive ana hhhh"
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+# oh-my-zsh-auto
