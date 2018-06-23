be7c697d94d0cba08e00b9d531eb54c52a2d8018
Termux <36556358+termux4me@users.noreply.github.com>
2018-06-23 01:27:08 +0100

    Add files via upload

--- banner.sh
+++ banner.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 cd /data/data/com.termux/files/usr/etc
 apt install neofetch
-echo"clear">>zshrc
-echo "neofetch">zshrc
-cp -rf motd /data/data/com.termux/files/usr/etc/fonts
-rm -rf motd
+echo "clear">zshrc
+echo "neofetch">>zshrc
+echo "cd /data/data/com.termux/files/usr/etc">>zshrc
+echo "cat banner.conf">>zshrc
+echo "Г ............................................................">banner.conf
+echo "Г  Пп_Пп_пПпп_ПпПпПП_-Пп_Пп_пПпп_ПпПпПП_-Пп_Пп_пПпп_ПпПпПП   :">>banner.conf
+echo "Г  п_Пhttps://github.com/termux4me/oh-my-zsh-autoП_ПпПпППП   :">>banner.conf
+echo "Г  Пп_Пп_пПпп_ПпПпПП_-Пп_Пп_пПпп_ПпПпПП_-Пп_Пп_пПпп_ПпПпПП   :">>banner.conf
+echo "Г ...........................................................:">>banner.conf
+echo "cd $HOME">>zshrc
+
--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,7 +1,12 @@
 #!/bash/usr/bin/env sh
+red='\033[1;31m'
+green='\033[1;32m'
+printf"$green"
 echo "DZ@SHELL"
+printf"$red"
 echo "setting up banner"
 sh banner.sh
+apt install -y curl
 sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
 # oh-my-zsh-auto
 #ooh yeaaaaah
